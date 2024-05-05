00792ef6e5ded110f7f3c6c825228a865405da0b
Bek Johansson <thedev.bek@gmail.com>
2024-05-05 13:53:44 -0500

    update home

---
+++ src/scripts/menu.js
@@ -0,0 +1,20 @@
+
+
+
+    document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('expanded');
+  });
+
+
+  // const nav = document.querySelector(".navbar");
+  // const navLinks = nav.querySelectorAll("a");
+  // const currentURL = window.location.href;
+  // navLinks.forEach((link) => {
+  //     if (link.href === currentURL) {
+  //         link.classList.add("text-pink-600"); // Add text-pink-600 class
+  //         link.classList.add("active"); // Optionally add an 'active' class
+  //     } else {
+  //         // Remove text-pink-600 class from other links (if previously set)
+  //         link.classList.remove("text-pink-600");
+  //     }
+  // });
